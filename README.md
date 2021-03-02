e4f668dcff8522e2cbebf1c8b67579e0bdb59b8f
John Hinojosa <hinojohn@ymail.com>
2021-03-01 23:27:25 -0600

    fixed issue saving to localStorage, changed condition to altering textarea bg color, added 2 screenshots of webpage, updated README

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 ## Description
     A simple in-browser calendar application, that allows user to enter hourly events, using Moment.js library  
     as well as dynamic HTML and CSS by use of jQuery.
+
+## Site Screenshot
+   ![Site Screenshot](assets/images/schedule_img1.png)
+   ![Site Screenshot](assets/images/schedule_img2.png)
+
+## Site Link
+   Link to [Work Day Scheduler](https://takolad.github.io/hw5-Work_Day_Scheduler/).
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var timeBlockContainerEl = $('.container')
 var saveBtnEl = $('.saveBtn');
 var textareaFieldEl = $('.description > textarea')
 
-// object to hold hour and text entered from textarea // considering removing/changing hour
+// object to hold hour and text entered from textarea // hour is not greatly being used atm
 var timeBlockObj = [
     {
         hour: 0,
@@ -43,19 +43,28 @@ var timeBlockObj = [
     },
 ];
 
-// localStorage
+
+/**********************
+//   localStorage    //
+**********************/
+
+// attempts to pull from localStorage and assign to a temporary variable
 var savedTimeBlock = JSON.parse(localStorage.getItem('schedule'));
 if(typeof(savedTimeBlock) !== 'undefined' && savedTimeBlock != null) {
     timeBlockObj = savedTimeBlock;
-
+    // sets textarea fields to their saved (in localStorage) values
     for (var i = 0; i < savedTimeBlock.length; i++) {
         var textareaEl = $(textareaFieldEl.get(i));
         if (savedTimeBlock[i].hour !== 0) {
             textareaEl.val(savedTimeBlock[i].text);
         }
     }
-};
+} else {
+    savedTimeBlock = timeBlockObj;
+}
 
+
+// Once DOM is ready, gets/displays date, appends save icons and handle button click
 $(document).ready(function (){
     var today = moment().format('dddd, MMMM Do');
     dateInputEl.text(today);
@@ -63,26 +72,20 @@ $(document).ready(function (){
     var saveIcon = $('<img src="assets/images/save.svg" alt="Save Icon"/>');
     saveBtnEl.append(saveIcon);
 
-    if (localStorage.getItem('schedule') !== null) {
-        
-    }
-
-    // event handling
+    // click event handler
     saveBtnEl.on('click', function(event) {
 
         var targetBlock = $(event.currentTarget).attr('btnNum');
         var index = targetBlock - 9;
-        var userEntry = textareaFieldEl.get(index).value;
+        var userEntry = textareaFieldEl.get(index).value.trim();
 
         timeBlockObj[index].hour = targetBlock;
         timeBlockObj[index].text = userEntry;
 
         // console.log(timeBlockObj);
-        savedTimeBlock.concat(timeBlockObj).unique;
+        savedTimeBlock.concat(timeBlockObj);
         localStorage.setItem('schedule', JSON.stringify(savedTimeBlock));
     });
-    
-
 });
 
 //***************************************************//
@@ -93,24 +96,24 @@ $(document).ready(function (){
 var time = moment().format('k');
 
 // For testing outside of work hours
-// var time = 11;
+// var time = 9;
 
-if (time >= 9 && time <= 17) {
-    // Checks each textarea's hour attribute and adds class based of comparison to momentjs time
-    $('textarea').each(function() {
-        // if hour is less than NOW - gray
-        if ($(this).attr("hour") < time) {
-            $(this).addClass('past');
-        }
-        // if hour is equal to NOW - green
-        else if ($(this).attr('hour') == time) {
-            $(this).addClass('present');
-        }
-        
-        // if hour is more than NOW - red
-        else if ($(this).attr('hour') > time) {
-            $(this).addClass('future');
-        }
-    })
+// Checks each textarea's hour attribute and adds class based of comparison to momentjs time
+$('textarea').each(function() {
+    // if hour is less than NOW - gray
+    if ($(this).attr("hour") < time) {
+        $(this).addClass('past');
+    }
+    // if hour is equal to NOW - green
+    else if ($(this).attr('hour') == time) {
+        $(this).addClass('present');
+    }
     
-    };
+    // if hour is more than NOW - red
+    else if ($(this).attr('hour') > time) {
+        $(this).addClass('future');
+    }
+})
+
+
+
